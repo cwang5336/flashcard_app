576f6a1f0951fd389d95e1e556adb046eb9d5158
cwang5336 <cwang5336@gmail.com>
2021-02-20 14:56:07 -0500

    added a click listener and answer question textview

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //define the TextViews im using
+        TextView flashcardAnswer = ((TextView) findViewById(R.id.flashcard_answer));
+        TextView flashcardQuestion = ((TextView) findViewById(R.id.flashcard_question));
+
+
+        //when click on flashcard_question it will make question invisible and make answer visible
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something here
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        //when click on answer it will make question visible and make question visible
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something here
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
     }
+
 }
